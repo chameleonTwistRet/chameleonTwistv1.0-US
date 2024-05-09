ae530e55ff056535b1aa907de7ddbc563b348a55
Nathan-R-Og <nathanminer923@gmail.com>
2024-05-09 16:11:05 -0500

    my b

--- src/code/vector.c
+++ src/code/vector.c
@@ -7,45 +7,45 @@ extern char D_8011C6E0; // "%7.1f %7.1f %7.1f "
 void DummiedPrintf3(char* arg0, ...) {
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D057C.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D057C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D05E0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D05E0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D06B4.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D06B4.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D0A9C.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D0A9C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D0AE4.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D0AE4.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D0B4C.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D0B4C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D0C44.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D0C44.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D0D3C.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D0D3C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D0E3C.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D0E3C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D0F10.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D0F10.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D0FE0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D0FE0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D1130.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D1130.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D11B0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D11B0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D1214.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D1214.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D1278.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D1278.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D1298.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D1298.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D12B0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D12B0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D1344.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D1344.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D1438.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D1438.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/AB8D0/func_800D1568.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/code/vector/func_800D1568.s")
 
 /**
  * @brief Prints a vector to the console.
